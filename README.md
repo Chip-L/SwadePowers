1e8cc37965c0c0551e8f3c2dff6683fc172cb2d4
Chip Long <chip_long@yahoo.com>
2024-12-11 14:30:28 -0700

    fixing powercard css (in progress)

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This is the beginning of a character sheet. It is meant to give the user the abi
 
 - Filter by book
 - Add actions
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+The easiest way to deploy your app is to use the Vercel Platform.
--- client/src/containers/PowerCard/PowerCard.tsx
+++ client/src/containers/PowerCard/PowerCard.tsx
@@ -1,5 +1,6 @@
 import { capitalize } from "lodash";
 import { Power } from "@/types";
+import "./PowerCard.css";
 
 interface PowerCardProps {
   selectedPower: Power;
@@ -7,35 +8,32 @@ interface PowerCardProps {
 
 function PowerCard({ selectedPower }: PowerCardProps) {
   return (
-    <>
-      <div>
-        <h2>{selectedPower.name}</h2>
-        <div className="flex-container">
-          <p>Rank: {capitalize(selectedPower.rank ?? "Error")}</p>
+    <div className="PowerCard">
+      <h2>{selectedPower.name}</h2>
+      <div className="flex-container">
+        <p>Rank: {capitalize(selectedPower.rank ?? "Error")}</p>
 
-          <p>Power Points: {selectedPower.powerPoints}</p>
-          <p>
-            Range: {selectedPower.range}{" "}
-            {selectedPower.rangeMultiplier
-              ? "x" + selectedPower.rangeMultiplier
-              : selectedPower.rangeMultiplier}
-          </p>
-          <p>
-            Duration: {selectedPower.durationMultiplier}{" "}
-            {selectedPower.duration}
-          </p>
-        </div>
+        <p>Power Points: {selectedPower.powerPoints}</p>
         <p>
-          Trappings:{" "}
-          {(
-            selectedPower?.trappings?.map((trapping) =>
-              capitalize(trapping ?? ""),
-            ) ?? []
-          ).join(", ")}
+          Range: {selectedPower.range}{" "}
+          {selectedPower.rangeMultiplier
+            ? "x" + selectedPower.rangeMultiplier
+            : selectedPower.rangeMultiplier}
         </p>
-        <p>{selectedPower.fullDescription}</p>
+        <p>
+          Duration: {selectedPower.durationMultiplier} {selectedPower.duration}
+        </p>
       </div>
-    </>
+      <p>
+        Trappings:{" "}
+        {(
+          selectedPower?.trappings?.map((trapping) =>
+            capitalize(trapping ?? ""),
+          ) ?? []
+        ).join(", ")}
+      </p>
+      <p>{selectedPower.fullDescription}</p>
+    </div>
   );
 }
 
