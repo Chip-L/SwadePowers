db09367ad6804cd4f068e02f389f41b5a2848f27
Chip Long <chip_long@yahoo.com>
2025-01-29 19:29:06 -0700

    updated frontend

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
-# Savage Worlds Powers
+"# Savage Worlds Adventurer Edition Dynamic Lookup app
 
-This is the beginning of a character sheet. It is meant to give the user the ability to look up the different powers used in Savage worlds.
+## Description
+
+This app will just supply some quick lookup stuff for the spells, skills, and edges. The data will come from a BE GraphQL database.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies](#technologies)
+- [Credits](#credits)
+- [License](#license)
+- [Tests](#tests)
+
+## Installation
+
+Dependencies are locked with a `yarn.lock` file, so please use `yarn` instead of `npm` to install them.
+
+`yarn install`
+
+## Usage
+
+This uses yarn workspaces, so running the code in the root will run these commands for both the client and server.
+
+- `yarn dev` runs the code in developer mode
+- `yarn test` runs the test cases (in watch-mode by default)
+- `yarn generate` creates an updated types via CodeGen for the GraphQL artifacts
+- `yarn build` builds the code for distribution
+- `yarn prod` runs the code in production mode
+- `yarn coverage` runs the tests and displays the coverage of the test cases
+- `yarn lint` runs the linter
+- `yarn stage` runs the linter, does the build, and runs the tests
+
+## Technologies
+
+- React
+- TypeScript
+- Vite
+- Vitest
+- Apollo Server
+- CodeGen -- manages types between BE and FE
+
+## Credits
+
+This is using data from Pinnacle Entertainment Group. I do not have authorization.
+
+## License
+
+MIT
+
+## Tests
+
+All logic should have test cases.
+
+We expect a coverage of:
+
+    lines: 85%
+    branches: 70%
+    functions: 70%
+    statements: 85%"
 
 ## Future enhancements:
 
--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,55 +1,55 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:testing-library/react',
-        'plugin:jest-dom/recommended',
-        'plugin:sonarjs/recommended-legacy',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:testing-library/react",
+    "plugin:jest-dom/recommended",
+    "plugin:sonarjs/recommended-legacy",
+  ],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "vite.config.ts",
+    "codegen.ts",
+    "tests",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-refresh",
+    "lodash",
+    "testing-library",
+    "jest-dom",
+    "sonarjs",
+  ],
+  settings: { react: { version: "detect" } },
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": ["error", { skipUndeclared: true }],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
     ],
-    ignorePatterns: [
-        'dist',
-        '.eslintrc.cjs',
-        'vite.config.ts',
-        'codegen.ts',
-        'tests',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: true,
+    "lodash/import-scope": "warn",
+    "testing-library/prefer-user-event": "warn",
+    "testing-library/prefer-explicit-assert": "warn",
+  },
+  overrides: [
+    {
+      files: ["*.test.*"],
+      rules: {
+        // Disabling this so we don't have to extract repeated `it` descriptions into variables
+        "sonarjs/no-duplicate-string": "off",
+      },
     },
-    plugins: [
-        '@typescript-eslint',
-        'react',
-        'react-refresh',
-        'lodash',
-        'testing-library',
-        'jest-dom',
-        'sonarjs',
-    ],
-    settings: { react: { version: 'detect' } },
-    rules: {
-        'no-console': ['error', { allow: ['warn', 'error'] }],
-        'react/react-in-jsx-scope': 'off',
-        'react/prop-types': ['error', { skipUndeclared: true }],
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-        'lodash/import-scope': 'warn',
-        'testing-library/prefer-user-event': 'warn',
-        'testing-library/prefer-explicit-assert': 'warn',
-    },
-    overrides: [
-        {
-            files: ['*.test.*'],
-            rules: {
-                // Disabling this so we don't have to extract repeated `it` descriptions into variables
-                'sonarjs/no-duplicate-string': 'off',
-            },
-        },
-    ],
+  ],
 };
